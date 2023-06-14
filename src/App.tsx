9756aee24c3071b6348e1eacd6a8c2a6c8500965
Robert <robert@rivne.itstep.org>
2023-06-14 21:09:10 +0300

    added routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,15 @@
 import './App.css'
 
+import {BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
 function App() {
 
   return (
-    <h1>Team</h1> 
-    <h3>Nazar's work</h3>
-    <h3>Yevheniia Ustyk</h3>
-    <h3>Robert Barnabishvili</h3>
-    <h3>Dima Maksymiuk</h3>
-    <h3>Bohdan Yatsyk</h3>
-    <h3>Ivan Yakovchuk</h3>
-    <h3>Tkachuk Oleksandr</h3>
-    <h3>Eugene</h3> 
+    <Router>
+      <Routes>
+        <Route path='/' />
+      </Routes>
+    </Router>
   )
 }
 
