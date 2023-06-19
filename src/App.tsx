b8d4b4991459e7f3257c7aa8e0489001b98c8531
Robert <robert@rivne.itstep.org>
2023-06-19 19:24:16 +0300

    added Admin page and Sidebar component

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,16 @@ import './App.css'
 
 import {BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
+import Admin from './pages/Admin/Admin'
+
 function App() {
 
   return (
     <Router>
       <Routes>
-        <Route path='/' />
+        <Route path='/admin' element={<Admin/>}>
+          <Route path='profile' element={<Admin/>}/>
+        </Route>
       </Routes>
     </Router>
   )
