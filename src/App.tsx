844903f0872337632f1ab51ada3ceae971966d98
ek-hb <eugene.kalichak@gmail.com>
2023-06-21 18:27:59 +0300

    usersList commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,19 @@
-import './App.css'
+import "./App.css";
 
-import {BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import Admin from './pages/Admin/Admin'
-
+import Admin from "./pages/Admin/Admin";
+import Userslist from "./pages/UsersList/UsersList";
 function App() {
-
   return (
     <Router>
       <Routes>
-        <Route path='/admin' element={<Admin/>}>
-          <Route path='profile' element={<Admin/>}/>
+        <Route path="/admin" element={<Admin />}>
+          <Route path="usersList" element={<Userslist />} />
         </Route>
       </Routes>
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/pages/Admin/Admin.tsx
+++ src/pages/Admin/Admin.tsx
@@ -1,20 +1,19 @@
-import Sidebar from "../../components/Sidebar/Sidebar"
+import Sidebar from "../../components/Sidebar/Sidebar";
 
-
-import Box from '@mui/material/Box';
-import Grid from '@mui/material/Grid';
-
-
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
+import Userslist from "../UsersList/UsersList";
 
 export default function Admin() {
   return (
     <Box sx={{ flexGrow: 1 }}>
       <Grid container spacing={2}>
-        <Grid item xs={3}>
-        <Sidebar/>
+        <Grid item xs={3.5}>
+          <Sidebar />
         </Grid>
-        <Grid item xs={9}>
-            <h1>Admin</h1>
+        <Grid item xs={8}>
+          <h1>Admin</h1>
+          <Userslist />
         </Grid>
       </Grid>
     </Box>
