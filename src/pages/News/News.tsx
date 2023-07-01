dcf2d46390276282ac814cc43293b4d5efb7b8c6
litvinovjobs@gmail.com <litvinovjobs@gmail.com>
2023-07-01 22:41:21 +0300

    finish coding pages News

--- src/pages/News/News.tsx
+++ src/pages/News/News.tsx
@@ -3,40 +3,40 @@ import "./News.css";
 export default function News() {
   return (
     <>
-      <h2>News</h2>
       <div className="block__add-news">
-        <input type="text" />
-        <button>Add news</button>
+        <textarea name="" id="" placeholder="Enter the article"></textarea>
+        <button>Send</button>
       </div>
+      <hr />
       <div className="wrapper">
         <div className="block__news">
-          <div className="block__img">
-            <img src="https://pi.tedcdn.com/r/talkstar-photos.s3.amazonaws.com/uploads/72bda89f-9bbf-4685-910a-2f151c4f3a8a/NicolaSturgeon_2019T-embed.jpg?w=512" alt="img" />
-            <p className="news__title">Як виник світ?</p>
-          </div>
+          <p className="news__title">How did the world originate?</p>
+          <p className="data_news">01.07.23 : 12:40 PM</p>
           <aside>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus, aperiam provident nulla, ipsam facere molestiae impedit velit labore accusamus fuga magnam excepturi quidem ad minus reprehenderit modi aspernatur temporibus? Optio non laborum aperiam? Velit ullam, quae blanditiis, iure atque nisi cupiditate dolorum porro accusamus dolor autem nemo? Blanditiis aut ex obcaecati, repellendus enim quidem cupiditate. Commodi veritatis, neque corrupti officiis aspernatur repellat explicabo? Ex omnis totam, aliquid assumenda quia repudiandae incidunt. Minus illum sit pariatur sequi soluta neque repellendus aut praesentium aperiam fuga cum illo ea earum repellat, ex ducimus dolore laborum doloremque eligendi, sapiente debitis, labore exercitationem aliquid saepe. Nobis ex aliquid unde facilis recusandae dolores labore sunt ipsum animi, numquam qui ea. Totam assumenda eaque sint voluptatum autem. Repellendus reiciendis quaerat adipisci necessitatibus, quidem reprehenderit amet ratione quasi molestias ea a deserunt porro debitis itaque dolores hic odit eos dolorem! Non, veritatis architecto, explicabo, deleniti aperiam ducimus assumenda illo corrupti quos dignissimos earum voluptates reiciendis. Fugiat ex incidunt quas aspernatur maxime! Ea placeat consequuntur facere quaerat illum voluptate id hic, veritatis provident eos aut rerum officia nemo dolorum aspernatur omnis sapiente, vel sequi impedit quos. Aspernatur, cumque numquam. Praesentium molestias, aliquam porro qui sapiente et ullam minima fugiat?
           </aside>
         </div>
         <div className="block__news">
-          <div className="block__img">
-            <img src="https://pi.tedcdn.com/r/talkstar-photos.s3.amazonaws.com/uploads/72bda89f-9bbf-4685-910a-2f151c4f3a8a/NicolaSturgeon_2019T-embed.jpg?w=512" alt="img" />
-            <p className="news__title">Як виник світ?</p>
-          </div>
+          <p className="news__title">How did the world originate?</p>
+          <p className="data_news">01.07.23 : 12:40 PM</p>
           <aside>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus, aperiam provident nulla, ipsam facere molestiae impedit velit labore accusamus fuga magnam excepturi quidem ad minus reprehenderit modi aspernatur temporibus? Optio non laborum aperiam? Velit ullam, quae blanditiis, iure atque nisi cupiditate dolorum porro accusamus dolor autem nemo? Blanditiis aut ex obcaecati, repellendus enim quidem cupiditate. Commodi veritatis, neque corrupti officiis aspernatur repellat explicabo? Ex omnis totam, aliquid assumenda quia repudiandae incidunt. Minus illum sit pariatur sequi soluta neque repellendus aut praesentium aperiam fuga cum illo ea earum repellat, ex ducimus dolore laborum doloremque eligendi, sapiente debitis, labore exercitationem aliquid saepe. Nobis ex aliquid unde facilis recusandae dolores labore sunt ipsum animi, numquam qui ea. Totam assumenda eaque sint voluptatum autem. Repellendus reiciendis quaerat adipisci necessitatibus, quidem reprehenderit amet ratione quasi molestias ea a deserunt porro debitis itaque dolores hic odit eos dolorem! Non, veritatis architecto, explicabo, deleniti aperiam ducimus assumenda illo corrupti quos dignissimos earum voluptates reiciendis. Fugiat ex incidunt quas aspernatur maxime! Ea placeat consequuntur facere quaerat illum voluptate id hic, veritatis provident eos aut rerum officia nemo dolorum aspernatur omnis sapiente, vel sequi impedit quos. Aspernatur, cumque numquam. Praesentium molestias, aliquam porro qui sapiente et ullam minima fugiat?
           </aside>
         </div>
         <div className="block__news">
-          <div className="block__img">
-            <img src="https://pi.tedcdn.com/r/talkstar-photos.s3.amazonaws.com/uploads/72bda89f-9bbf-4685-910a-2f151c4f3a8a/NicolaSturgeon_2019T-embed.jpg?w=512" alt="img" />
-            <p className="news__title">Як виник світ?</p>
-          </div>
+          <p className="news__title">How did the world originate?</p>
+          <p className="data_news">01.07.23 : 12:40 PM</p>
           <aside>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus, aperiam provident nulla, ipsam facere molestiae impedit velit labore accusamus fuga magnam excepturi quidem ad minus reprehenderit modi aspernatur temporibus? Optio non laborum aperiam? Velit ullam, quae blanditiis, iure atque nisi cupiditate dolorum porro accusamus dolor autem nemo? Blanditiis aut ex obcaecati, repellendus enim quidem cupiditate. Commodi veritatis, neque corrupti officiis aspernatur repellat explicabo? Ex omnis totam, aliquid assumenda quia repudiandae incidunt. Minus illum sit pariatur sequi soluta neque repellendus aut praesentium aperiam fuga cum illo ea earum repellat, ex ducimus dolore laborum doloremque eligendi, sapiente debitis, labore exercitationem aliquid saepe. Nobis ex aliquid unde facilis recusandae dolores labore sunt ipsum animi, numquam qui ea. Totam assumenda eaque sint voluptatum autem. Repellendus reiciendis quaerat adipisci necessitatibus, quidem reprehenderit amet ratione quasi molestias ea a deserunt porro debitis itaque dolores hic odit eos dolorem! Non, veritatis architecto, explicabo, deleniti aperiam ducimus assumenda illo corrupti quos dignissimos earum voluptates reiciendis. Fugiat ex incidunt quas aspernatur maxime! Ea placeat consequuntur facere quaerat illum voluptate id hic, veritatis provident eos aut rerum officia nemo dolorum aspernatur omnis sapiente, vel sequi impedit quos. Aspernatur, cumque numquam. Praesentium molestias, aliquam porro qui sapiente et ullam minima fugiat?
           </aside>
         </div>
-
+        <div className="block__news">
+          <p className="news__title">How did the world originate?</p>
+          <p className="data_news">01.07.23 : 12:40 PM</p>
+          <aside>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus, aperiam provident nulla, ipsam facere molestiae impedit velit labore accusamus fuga magnam excepturi quidem ad minus reprehenderit modi aspernatur temporibus? Optio non laborum aperiam? Velit ullam, quae blanditiis, iure atque nisi cupiditate dolorum porro accusamus dolor autem nemo? Blanditiis aut ex obcaecati, repellendus enim quidem cupiditate. Commodi veritatis, neque corrupti officiis aspernatur repellat explicabo? Ex omnis totam, aliquid assumenda quia repudiandae incidunt. Minus illum sit pariatur sequi soluta neque repellendus aut praesentium aperiam fuga cum illo ea earum repellat, ex ducimus dolore laborum doloremque eligendi, sapiente debitis, labore exercitationem aliquid saepe. Nobis ex aliquid unde facilis recusandae dolores labore sunt ipsum animi, numquam qui ea. Totam assumenda eaque sint voluptatum autem. Repellendus reiciendis quaerat adipisci necessitatibus, quidem reprehenderit amet ratione quasi molestias ea a deserunt porro debitis itaque dolores hic odit eos dolorem! Non, veritatis architecto, explicabo, deleniti aperiam ducimus assumenda illo corrupti quos dignissimos earum voluptates reiciendis. Fugiat ex incidunt quas aspernatur maxime! Ea placeat consequuntur facere quaerat illum voluptate id hic, veritatis provident eos aut rerum officia nemo dolorum aspernatur omnis sapiente, vel sequi impedit quos. Aspernatur, cumque numquam. Praesentium molestias, aliquam porro qui sapiente et ullam minima fugiat?
+          </aside>
+        </div>
       </div>
     </>
   );
