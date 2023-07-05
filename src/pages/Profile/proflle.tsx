b563760164927bc747fed9dc9433442bcf74f4e2
dimamaksymiuk01 <admin@admins-MacBook-Air.local>
2023-07-05 20:55:18 +0300

    logicProfile_V1.4

--- src/pages/Profile/proflle.tsx
+++ src/pages/Profile/proflle.tsx
@@ -1,16 +1,40 @@
-import './profile.css'
-
 import Button from '@mui/material/Button';
 import Avatar from '@mui/material/Avatar';
 import Stack from '@mui/material/Stack';
+import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { getSelectedUser} from "../../common/utils/localStorageLogic.ts";
 
-import {Link} from "react-router-dom";
+import './profile.css'
+
+interface logicSelectedUser {
+  id: string;
+  Name: string;
+  Surname: string;
+  Email: string;
+  PhoneNumber: number;
+  EmailConfirmed: boolean;
+  lockoutEnd: void; //хз шо це має бути, тому поки так))
+  role: string;
+}
+
 
 export default function Profile() {
+
+  const [user, setUser] = useState<logicSelectedUser>();
+
+  useEffect(() => {
+    const selectedUser = getSelectedUser();
+    setUser(selectedUser);
+  }, []);
+
+  console.log(user);
+
+
   return (
           <div className={"box"}>
     <div className="card-container">
-      <span className="pro">Admin</span>
+      <span className="pro">{user?.role}</span>
       <div className={"av"}>
       <Stack direction="row" spacing={2}>
       <Avatar className={"avatar"}
@@ -20,7 +44,7 @@ export default function Profile() {
       />
     </Stack>
         </div>
-      <h3>Ricky Park</h3>
+      <h3>{user?.Name} {user?.Surname}</h3>
       <h6>Ukraine</h6>
       <p>Chief manager in the company: The one who makes all the important decisions and takes full responsibility... for blaming others when things go wrong.</p>
       <div className="buttons">
@@ -33,10 +57,12 @@ export default function Profile() {
       <div className="skills">
         <h6>Info</h6>
         <ul>
-          <li>Email: admin@email.com</li>
-          <li>Address: Ukraine, Rivne</li>
-          <li>Password: w*******</li>
-          <li>Phone: 389 97 055 3072</li>
+            <li>Email: {user?.Email}</li>
+            <li>Phone: {user?.PhoneNumber}</li>
+                      <li>Name: {user?.Name}</li>
+                      <li>Surname: {user?.Surname}</li>
+            <li>Role: {user?.role}</li>
+
         </ul>
       </div>
     </div>
@@ -44,9 +70,3 @@ export default function Profile() {
           </div>
   );
 }
-
-
-
-
-
-//
