f4663c7e2a24d07964848c7cf061a7cccf884ac5
Robert <robert@rivne.itstep.org>
2023-07-10 21:10:08 +0300

    Error2

--- src/hocs/WithAuth.tsx
+++ src/hocs/WithAuth.tsx
@@ -19,10 +19,9 @@ export const withAuth = (WrappedComponent: ComponentType<AuthProps>) => {
     const [isLoading, setIsLoading] = useState(true);
 
     useEffect(() => {
-      getAllUsers(0, 0, true).then(({response} ) => {
-        console.log(response);
-        
-        if (response?.isSuccess) {
+      getAllUsers(0, 0, true).then((response ) => {
+      
+        if (response?.response.isSuccess) {
           setIsAuthenticated(true);
         } else {
           setIsAuthenticated(false);
