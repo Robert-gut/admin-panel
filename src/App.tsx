3cd80c417f1fd0a6cfdb2f4298c7cd4b3c2a8e75
dimamaksymiuk01 <admin@admins-MacBook-Air.local>
2023-07-10 20:03:36 +0300

    resolve conflict

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import "./App.css";
 import { Routes, Route, Navigate } from "react-router-dom";
 
 import Nope from "./pages/NotFound/Nope";
-import Admin from "./layouts/Admin";s
+import Admin from "./layouts/Admin";
 
 import Login from "../src/pages/Auth/Login";
 import ForgotPassword from "./pages/Auth/ForgotPassword/ForgotPassword";
