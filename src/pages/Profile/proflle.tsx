58bd0909ad01b39959de70cdc420ec56540c70e7
ek-hb <eugene.kalichak@gmail.com>
2023-07-17 20:03:20 +0300

    consoleCommit

--- src/pages/Profile/proflle.tsx
+++ src/pages/Profile/proflle.tsx
@@ -1,14 +1,13 @@
-import Button from '@mui/material/Button';
-import Avatar from '@mui/material/Avatar';
-import Stack from '@mui/material/Stack';
+import Button from "@mui/material/Button";
+import Avatar from "@mui/material/Avatar";
+import Stack from "@mui/material/Stack";
 import { Link } from "react-router-dom";
 import { useState, useEffect } from "react";
-import { getSelectedUser} from "../../common/utils/localStorageLogic.ts";
-import {logicSelectedUser} from "../EditProfile/typeProfiles.ts";
-import './profile.css'
+import { getSelectedUser } from "../../common/utils/localStorageLogic.ts";
+import { logicSelectedUser } from "../EditProfile/typeProfiles.ts";
+import "./profile.css";
 
 export default function Profile() {
-
   const [user, setUser] = useState<logicSelectedUser>();
 
   useEffect(() => {
@@ -16,56 +15,59 @@ export default function Profile() {
     setUser(selectedUser);
   }, []);
 
-  console.log(user);
+  // Додаємо підписку на подію storage, що виникає при зміні стану локального сховища
+  const handleStorageChange = (event: StorageEvent) => {
+    if (event.key === "selectedUser") {
+      const updatedUser = JSON.parse(
+        event.newValue || "{}"
+      ) as logicSelectedUser;
+      setUser(updatedUser);
+    }
+  };
 
-      // Додаємо підписку на подію storage, що виникає при зміні стану локального сховища
-    const handleStorageChange = (event: StorageEvent) => {
-      if (event.key === 'selectedUser') {
-        const updatedUser = JSON.parse(event.newValue || '{}') as logicSelectedUser;
-        setUser(updatedUser);
-      }
-    };
-
-    window.addEventListener('storage', handleStorageChange);
-
-
+  window.addEventListener("storage", handleStorageChange);
 
   return (
-          <div className={"box"}>
-    <div className="card-container">
-      <span className="pro">{user?.role}</span>
-      <div className={"av"}>
-      <Stack direction="row" spacing={2}>
-      <Avatar className={"avatar"}
-        alt="Remy Sharp"
-        src="https://www.training.com.au/wp-content/uploads/admin-worker-vector.png"
-        sx={{ width: 100, height: 100 }}
-      />
-    </Stack>
+    <div className={"box"}>
+      <div className="card-container">
+        <span className="pro">{user?.role}</span>
+        <div className={"av"}>
+          <Stack direction="row" spacing={2}>
+            <Avatar
+              className={"avatar"}
+              alt="Remy Sharp"
+              src="https://www.training.com.au/wp-content/uploads/admin-worker-vector.png"
+              sx={{ width: 100, height: 100 }}
+            />
+          </Stack>
         </div>
-      <h3>{user?.Name} {user?.Surname}</h3>
-      <h6>Ukraine</h6>
-      <p>Chief manager in the company: The one who makes all the important decisions and takes full responsibility... for blaming others when things go wrong.</p>
-      <div className="buttons">
-        <Link to={"/admin/edit"}>
-    <Button variant="contained" disableElevation>
-      Edit profile
-    </Button>
+        <h3>
+          {user?.Name} {user?.Surname}
+        </h3>
+        <h6>Ukraine</h6>
+        <p>
+          Chief manager in the company: The one who makes all the important
+          decisions and takes full responsibility... for blaming others when
+          things go wrong.
+        </p>
+        <div className="buttons">
+          <Link to={"/admin/edit"}>
+            <Button variant="contained" disableElevation>
+              Edit profile
+            </Button>
           </Link>
-      </div>
-      <div className="skills">
-        <h6>Info</h6>
-        <ul>
+        </div>
+        <div className="skills">
+          <h6>Info</h6>
+          <ul>
             <li>Email: {user?.Email}</li>
             <li>Phone: {user?.PhoneNumber}</li>
-                      <li>Name: {user?.Name}</li>
-                      <li>Surname: {user?.Surname}</li>
+            <li>Name: {user?.Name}</li>
+            <li>Surname: {user?.Surname}</li>
             <li>Role: {user?.role}</li>
-
-        </ul>
+          </ul>
+        </div>
       </div>
     </div>
-
-          </div>
   );
 }
